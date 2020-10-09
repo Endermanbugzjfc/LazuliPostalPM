99f31efb7804ea97a40d8a2b2e10249f30d43187
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2020-10-09 21:08:42 +0800

    09OCT2020 0908 (1)

--- src/Endermanbugzjfc/LazuliPostalPM/LazuliPostalPM.php
+++ src/Endermanbugzjfc/LazuliPostalPM/LazuliPostalPM.php
@@ -38,20 +38,36 @@ final class LazuliPostalPM extends \pocketmine\plugin\PluginBase {
 		foreach ($all as $k => $v) $conf->remove($k);
 		
 		$conf->set('enable-plugin', (bool)($all['enable-plugin'] ?? true));
-		foreach (($all['world-groups'] ?? [
-			default => [
+		$conf->set('maxium-mails-per-mailbox', (int)($all['maxium-mails-per-mailbox'] ?? 2147483647));
+		$conf->set('maxium-chars-per-letter', (int)($all['maxium-chars-per-letter'] ?? 2048));
+		$conf->set('world-groups', (array)array_map(function (array $all) : array {
+			$group = [];
+			if (isset($all['allow-secret-letter-copies'])) $group['allow-secret-letter-copies'] = (bool)$all['allow-secret-letter-copies'];
+			
+			if (isset($all['allow-secret-letter-replies'])) $group['allow-secret-letter-replies'] = (bool)$all['allow-secret-letter-replies'];
+			
+			if (isset($all['allow-secret-letter-replies'])) $group['allow-secret-letter-replies'] = (bool)$all['allow-secret-letter-replies'];
+			
+			return $group;
+		}, $all['world-groups'] ?? self::getDefaultWorldGroups()));
+		
+		$conf->save();
+		$conf->reload();
+		return (bool)$conf->get('enable-plugin', true);
+	}
+	
+	private static function getDefaultWorldGroups() : array {
+		return [
+			'global' => [
 				'allow-secret-letter-copies' => true,
 				'allow-secret-letter-replies' => true,
-				'allow-percels' => true,
-				'percel-items-blacklist' => []
-			],
-			[
-			skyblock-server => [
-				'allow-secret-letter-copies' => true,
-				'allow-secret-letter-replies' => true,
-				'allow-percels' => true
-			]
-		]) as $name =>
+				'allow-parcels' => true,
+				'parcel-items-blacklist' => [],
+				'parcel-items-whitelist' => [],
+				'default-parcel-item-price' => 0.0,
+				'specified-parcel-price' => [],
+				'maxium-items-per-parcel' => 64
+		];
 	}
 
 }
